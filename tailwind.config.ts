0094db670b80bc7a86072ed873f4cac1ab78e949
AdreshSingh <adreshsingh271527@gmail.com>
2024-10-08 05:03:36 +0530

    added personal theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,6 +13,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#202225",
+        secondary: "#5865f2"
       },
     },
   },
